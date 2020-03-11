7ba01d8c3a606fb18e3ee6fff4f1d8045a70fe99
ZachMartinez <martinez.zach.m@gmail.com>
2020-03-10 21:01:27 -0700

    Add Cell and gameboard objects

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Hi");
+const Cell = (value = null) => {
+  const getValue = () => value;
+  
+  const setValue = (newVal) => {
+    if (value == null)
+      value = newVal;
+  }
+  
+  return {
+    getValue,
+    setValue
+  }
+};
+
+const gameboard = (() => {
+  const cells = (() => {
+    let elements = document.querySelectorAll(".cell");
+
+    let cells = {};
+    elements.forEach((element) => {
+      cells[element.dataset.number] = Cell();
+    });
+
+    return cells;
+  })();
+
+  const cell = (number) => {
+    return cells[number];
+  };
+
+  return {
+    cell,
+  }
+})();
