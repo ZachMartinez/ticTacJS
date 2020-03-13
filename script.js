c18adc8201fb1d126e839ca2b62c495e5c542ce0
ZachMartinez <martinez.zach.m@gmail.com>
2020-03-13 10:20:23 -0700

    Add modules for player and game

--- script.js
+++ script.js
@@ -12,6 +12,53 @@ const Cell = (value = null) => {
   }
 };
 
+const Player = (name, mark) => {
+  const getName = () => name;
+  const getMark = () => mark;
+  
+  const markedCells = [];
+  const getMarkedCells = () => markedCells;
+
+  const winningPositions = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [7,5,3]
+  ];
+
+  const isWinner = () => {
+    let output = false;
+    const playerPosition = markedCells.sort();
+    winningPositions.forEach((position) => {
+      if (position.every((i) => playerPosition.includes(i)))
+        output = true;
+    });
+
+    return output;
+  };
+
+  const play = (pos) => {
+    markedCells.push(pos);
+
+    return {
+      mark: getMark(),
+      pos: pos
+    }
+  };
+
+  return {
+    getMarkedCells,
+    getName,
+    getMark,
+    play,
+    isWinner
+  }
+};
+
 const gameboard = (() => {
   const cells = (() => {
     let elements = document.querySelectorAll(".cell");
@@ -24,11 +71,24 @@ const gameboard = (() => {
     return cells;
   })();
 
-  const cell = (number) => {
-    return cells[number];
-  };
+  const cell = (number) => cells[number];
+  const totalCells = () => Object.keys(cells).length;
 
   return {
     cell,
+    totalCells,
+  }
+})();
+
+const game = (() => {
+  const playerX = Player("Human", "X");
+  const playerO = Player("Computer", "O");
+  const board = gameboard;
+  let winner;
+
+  return {
+    playerX,
+    playerO,
+    board,
   }
 })();
